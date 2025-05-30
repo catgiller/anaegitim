29890f14571bda0e446c3b3d97726a10cc937213
catgiller <49812422+catgiller@users.noreply.github.com>
2025-05-30 10:12:33 +0000

    lalala

---
+++ libft/ft_atoi.c
@@ -0,0 +1,28 @@
+#include "libft.h"
+
+int ft_atoi(const char *nptr)
+{
+	int	sig; 
+	int	rst;
+
+	rst = 0;
+	sig = 1;
+	while ((*nptr >= 9 && *nptr <= 13) || (*nptr == 32))
+		nptr++;
+	if(*nptr == '+')
+	{
+		sig = 1;
+		nptr ++;
+	}
+	else if (*nptr == '-')
+	{
+		sig = -1;
+		nptr ++;
+	}
+	while(*nptr >= 48 && *nptr <= 57)
+	{
+		rst = rst * 10 + (*nptr - '0');
+		nptr ++;
+	}
+	return(rst * sig);
+}
